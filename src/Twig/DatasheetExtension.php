d7670b914d01e6f3fa53c6888dfa9f5657098319
Alexander <alexander.advanced@gmail.com>
2023-11-02 20:10:19 +0100

    update

--- src/Twig/DatasheetExtension.php
+++ src/Twig/DatasheetExtension.php
@@ -2,9 +2,10 @@
 
 namespace AlexanderA2\SymfonyDatasheetBundle\Twig;
 
+use AlexanderA2\PhpDatasheet\Builder\DatasheetBuilder;
 use AlexanderA2\PhpDatasheet\DatasheetInterface;
-use AlexanderA2\PhpDatasheet\Factory\DatasheetBuilderFactory;
 use Symfony\Component\HttpFoundation\RequestStack;
+use Throwable;
 use Twig\Environment;
 use Twig\Extension\AbstractExtension;
 use Twig\TwigFunction;
@@ -12,8 +13,9 @@ use Twig\TwigFunction;
 class DatasheetExtension extends AbstractExtension
 {
     public function __construct(
-        protected Environment  $twig,
-        protected RequestStack $requestStack,
+        protected Environment      $twig,
+        protected RequestStack     $requestStack,
+        protected DatasheetBuilder $datasheetBuilder,
     ) {
     }
 
@@ -26,12 +28,20 @@ class DatasheetExtension extends AbstractExtension
 
     public function renderDatasheet(DatasheetInterface $datasheet)
     {
-        (new DatasheetBuilderFactory())
-            ->get()
-            ->build($datasheet, $this->requestStack->getMainRequest()->query->all($datasheet->getName()));
+        try {
+            $this->datasheetBuilder->build(
+                $datasheet,
+                $this->requestStack->getMainRequest()->query->all($datasheet->getName()),
+            );
 
-        return $this->twig->render('@Datasheet/layout.html.twig', [
-            'datasheet' => $datasheet,
-        ]);
+            return $this->twig->render('@Datasheet/layout.html.twig', [
+                'datasheet' => $datasheet,
+            ]);
+        } catch (Throwable $exception) {
+            return $this->twig->render('@Datasheet/exception.html.twig', [
+                'datasheet' => $datasheet,
+                'exception' => $exception,
+            ]);
+        }
     }
 }
