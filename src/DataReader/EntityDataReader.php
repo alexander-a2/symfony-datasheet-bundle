4267f9449e2278290c764362a0f0390295a2bb90
Alexander <alexander.advanced@gmail.com>
2023-11-04 10:09:45 +0100

    update

--- src/DataReader/EntityDataReader.php
+++ src/DataReader/EntityDataReader.php
@@ -5,8 +5,6 @@ namespace AlexanderA2\SymfonyDatasheetBundle\DataReader;
 use AlexanderA2\PhpDatasheet\DataReader\DataReaderInterface;
 use AlexanderA2\PhpDatasheet\DataReader\QueryBuilderDataReader;
 use AlexanderA2\PhpDatasheet\DatasheetInterface;
-use AlexanderA2\PhpDatasheet\Helper\EntityHelper;
-use AlexanderA2\PhpDatasheet\Helper\QueryBuilderHelper;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 
 class EntityDataReader extends QueryBuilderDataReader implements DataReaderInterface
@@ -14,30 +12,8 @@ class EntityDataReader extends QueryBuilderDataReader implements DataReaderInter
     public function setSource(mixed $source): self
     {
         parent::setSource($source->createQueryBuilder('e'));
-        $this->addRelations();
 
         return $this;
-    }
-
-    /**
-     * todo: check if relations are already added
-     * todo: check if selections are already added
-     */
-    protected function addRelations(): void
-    {
-        $primaryAlias = QueryBuilderHelper::getPrimaryAlias($this->getQueryBuilder());
-        $entityFields = EntityHelper::getEntityFields(
-            QueryBuilderHelper::getPrimaryClass($this->getQueryBuilder()),
-            $this->getQueryBuilder()->getEntityManager(),
-        );
-
-        foreach ($entityFields as $fieldName => $fieldType) {
-            if (!in_array($fieldType, EntityHelper::RELATION_FIELD_TYPES)) {
-                continue;
-            }
-            $joinAlias = $this->addJoin($primaryAlias, $fieldName);
-            $this->getQueryBuilder()->addSelect($joinAlias);
-        }
     }
 
     public static function supports(DatasheetInterface $datasheet): bool
